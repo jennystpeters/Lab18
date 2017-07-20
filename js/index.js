02629fcc668bc2f7a466c8995d94073fcb1a46a5
jennystpeters <jennystpeters@gmail.com>
2017-07-20 09:57:49 -0400

    First Commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+photoSurvey = document.getElementById("photoSurvey");
+
+function report(period) {
+  photoSurvey.innerHTML = "";
+  photoSurvey.innerHTML =
+    photoSurvey.innerHTML +
+    "Thank you for participating in our survey. Have a great day!";
+}
